b47d83d80fdc9a913a47513714f0daaeb59b9404
gonchigars <gonchigars@gmail.com>
2024-07-10 12:10:34 +0530

    asdf

---
+++ Users/gonch/Desktop/Batch3/javabasics/FirstProject1/src/First.java
@@ -0,0 +1,20 @@
+public class First {
+
+    public static void main(String[] args) {
+        
+        int Count = 10;
+        System.out.println("Count value "+Count);
+        Count =20;
+        System.out.println("Count value "+Count);
+        double interest = 5.8;
+        String name = "Shashank";
+        float interestf = 5.8f;
+
+        System.out.println("Interest rate is "+interestf);
+
+
+        
+        
+        
+    }
+}
