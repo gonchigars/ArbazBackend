aa7e96421a7b4ba25df4a9cc13fa41e818611d03
gonchigars <gonchigars@gmail.com>
2024-07-11 12:06:20 +0530

    basic project

---
+++ Users/gonch/Desktop/Batch3/javabasics/FirstProject/src/First.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Enter your Name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        }
+    
+}
